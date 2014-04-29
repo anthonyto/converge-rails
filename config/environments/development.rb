c37e9bda6abfdcbb9f3359ae00c89fa8d43d4304
Anthony To <to.anthony93@gmail.com>
2014-04-29 11:34:38 -0500

    Update AWS access key vulnerability

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,16 +34,10 @@ ConvergeRails::Application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      # :bucket => ENV['S3_BUCKET_NAME'],
-      # :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      # :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-      :bucket => 'converge-rails',
-      :access_key_id => 'AKIAI5HT72X2VYPCTS2A',
-      :secret_access_key => '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'
+      :bucket => ENV['S3_BUCKET_NAME'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
-  
-  #AK: AKIAJJVY7HLNLQXBISAA
-  #SAK: Xwqb3OuPQSTv9lxlHMDiVdxJZ06bqzO7BluRiAfd
   
 end
